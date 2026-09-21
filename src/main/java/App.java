48f6f0aaa0c7b5720390a691844e703758e27b74
Bishan Biswas <2022wc86058@wilp.bits-pilani.ac.in>
2026-09-21 07:42:57 +0530

    Modified source code for CI

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("DevOps Continuous Integration Demo");
+        System.out.println("DevOps Continuous Integration Demo - Version 2");
     }
 }
